fe305fcc4c5aab2333bba4d8e2726a89ce41eed3
epervago <evgeny@listo.mx>
2019-11-18 14:06:41 -0600

    Update django.py

--- celery/fixups/django.py
+++ celery/fixups/django.py
@@ -30,8 +30,9 @@ def _maybe_close_fd(fh):
 
 
 def _verify_django_version(django):
-    if django.VERSION < (1, 11):
-        raise ImproperlyConfigured('Celery 4.x requires Django 1.11 or later.')
+    pass
+#     if django.VERSION < (1, 11):
+#         raise ImproperlyConfigured('Celery 4.x requires Django 1.11 or later.')
 
 
 def fixup(app, env='DJANGO_SETTINGS_MODULE'):
